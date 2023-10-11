80fbb0733122b7b95af8801fb7af4688db92f820
thibault.huyghe <thibault_huyghe@mohawkind.com>
2023-10-11 20:21:38 +0200

    checkout dashboard

---
+++ Store.App/Store.Web/Pages/CheckoutDashboard.razor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+using Store.Shared.Dto;
+using Store.Web.Helpers.Modals;
+using Store.Web.Services;
+
+namespace Store.Web.Pages
+{
+    public partial class CheckoutDashboard
+    {
+        [Inject]
+        public IProductService ProductService { get; set; }
+
+        [Inject]
+
+        public ICategoryService CategoryService { get; set; }
+
+        private List<ProductDto>? _Products = new List<ProductDto>();
+
+        private List<CategoryDto>? _Categories = new List<CategoryDto>();
+
+        private CategoryDto _SelectedCategory = default!;
+
+        protected override async Task OnInitializedAsync()
+        {
+            _Products = (await ProductService.GetAllProducts()).ToList();
+            _Categories = (await CategoryService.GetAllCategorys()).ToList();
+
+            _SelectedCategory = _Categories.First();
+        }
+
+        private void SetSelectedCategroy(CategoryDto category)
+        {
+            _SelectedCategory = category;
+        }
+    }
+}
