33946dde98f9a58644a665834953808db144314d
thibault.huyghe <thibault_huyghe@mohawkind.com>
2023-10-25 22:07:55 +0200

    Adding a wizard WIP
    https://github.com/snickler/Samples/blob/master/BlazorWizard/Pages/WizardDemo.razor

---
+++ Store.App/Store.Web/Components/Wizard/WizardStep.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Store.Web.Components.Wizard
+{
+    public partial class WizardStep
+    {
+        [CascadingParameter]
+        protected internal Wizard Parent { get; set; }
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+
+        [Parameter]
+        public string Name { get; set; }
+
+
+
+        protected override void OnInitialized()
+        {
+            Parent.AddStep(this);
+        }
+    }
+}
